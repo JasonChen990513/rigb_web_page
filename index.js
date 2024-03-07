0b797c122cb73bfc08d84d7adff0d1f753881540
JasonChen990513 <149560212+JasonChen990513@users.noreply.github.com>
2024-03-07 18:27:15 +0800

    finish sliding part

---
+++ index.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    //direction: 'vertical',
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+  
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+
+  });
