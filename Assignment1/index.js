6a1c07c946f207bcbd318ae9cec00666a8888fe2
JasonChen990513 <149560212+JasonChen990513@users.noreply.github.com>
2024-03-08 18:27:54 +0800

    doing the responsive part

---
+++ Assignment1/index.js
@@ -0,0 +1,40 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  //direction: 'vertical',
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+
+});
+
+const faqs = document.querySelectorAll(".faqsBtn");
+faqs.forEach(faq => faq.addEventListener("click", (e)=>{
+  content = faq.querySelector(".faqContent")
+
+  if (content.classList.contains('hidden')) {
+    content.classList.remove('hidden');
+  } else {
+    content.classList.add('hidden');
+  }
+
+}));
+
+
+const menuIcon = document.querySelector('#menuIcon');
+const menu = document.querySelector('#menu');
+
+menuIcon.addEventListener('click', (e) => {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+});
