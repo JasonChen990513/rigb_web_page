d03918fb74a5b17a46685b2c4073fa37a67a405a
JasonChen990513 <149560212+JasonChen990513@users.noreply.github.com>
2024-03-11 18:14:12 +0800

    finish responsive part

--- Assignment1/index.js
+++ Assignment1/index.js
@@ -11,6 +11,18 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
+
+  breakpoints:{
+    0: {
+        slidesPerView: 1,
+    },
+    768: {
+        slidesPerView: 2,
+    },
+    1080: {
+        slidesPerView: 3,
+    },
+},
   
 
 });
