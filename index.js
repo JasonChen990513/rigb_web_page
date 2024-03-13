5bfb2675862e7d8f965aa909a9a7ec9a7489fb4a
JasonChen990513 <149560212+JasonChen990513@users.noreply.github.com>
2024-03-13 18:26:37 +0800

    update nav bar and some animation

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+//setting the card slider section
 const swiper = new Swiper('.swiper', {
   // Optional parameters
   //direction: 'vertical',
@@ -27,6 +28,132 @@ const swiper = new Swiper('.swiper', {
 
 });
 
+
+
+const faqs = document.querySelectorAll(".faqsBtn");
+
+//use to control the faq section show and hide content and buttom
+faqs.forEach(faq => faq.addEventListener("click", (e)=>{
+  content = faq.querySelector(".faqContent")
+
+  if (content.classList.contains('show')) {
+    content.style.animationName = "hide";
+    setTimeout(()=>{
+      content.classList.remove('show');
+    },500);
+    faq.querySelector(".showContent").classList.remove('hidden');
+    faq.querySelector(".hideContent").classList.add('hidden');
+  } else {
+    content.style.animationName = "show";
+    content.classList.add('show');
+    faq.querySelector(".showContent").classList.add('hidden');
+    faq.querySelector(".hideContent").classList.remove('hidden');
+  }
+
+}));
+
+
+const menuIcon = document.querySelector('#menuIcon');
+const menu = document.querySelector('#menu');
+const navbar = document.getElementById('navBar');
+
+//set event listener for menu animation and change background color
+menuIcon.addEventListener('click', (e) => {
+    if (menu.classList.contains('show')) {
+      document.querySelector("#menu").style.animationName = "menuHide";
+      navbar.style.backgroundColor = "";
+      setTimeout(()=>{
+        menu.classList.remove('show');
+      },500);
+      
+    } else {
+      document.querySelector("#menu").style.animationName = "menuShow";
+      menu.classList.add('show');
+      navbar.style.backgroundColor = "white";
+    }
+});
+//set event listener for screen size change
+window.addEventListener('resize', () => {
+  const screenWidth = window.innerWidth;
+  //hide the drop down menu for lx size screen
+  if(screenWidth >= 1280){
+    document.querySelector("#menu").style.animationName = "menuHide";
+    setTimeout(()=>{
+      menu.classList.remove('show');
+    },500);
+  }
+  setMainmt();
+  changeNavbg();
+});
+
+
+setMainmt();
+//set main mergin top. 
+function setMainmt(){
+  const navbar = document.getElementById('navBar');
+  const height = navbar.clientHeight;
+  const main = document.getElementById('main');
+  main.style.marginTop = (height+10) + "px" 
+  console.log(height)
+  console.log(navbar.style.top)
+  console.log(main.classList)
+}
+
+const element = document.getElementById('navBar');
+
+// Create a ResizeObserver instance
+const observer = new ResizeObserver(entries => {
+  for (let entry of entries) {
+    // Log the new height of the observed element
+    console.log('Height:', entry.contentRect.height);
+    
+    // Get the position of the special area relative to the viewport
+    const faqsectionAreaPosition = faqsection.getBoundingClientRect().top;
+
+    // Check if the top of the special area is visible in the viewport
+    if (faqsectionAreaPosition < navbar.clientHeight) {
+        // Change the background color when the special area is in view
+        navbar.style.backgroundColor = 'white';
+    }
+  }
+});
+
+// Observe the target element for size changes
+observer.observe(element);
+
+const faqsection = document.getElementById('faqsection');
+
+// Add a scroll event listener to the window
+window.addEventListener('scroll', function() {
+  changeNavbg();
+});
+
+function changeNavbg(){
+  const navbar = document.getElementById('navBar');
+  // Get the current scroll position
+  // const scrollPosition = window.scrollY;
+  // console.log('Scroll position:', scrollPosition);
+
+  // Get the position of the special area relative to the viewport
+  const faqsectionAreaPosition = faqsection.getBoundingClientRect().top;
+  //if the navbar is active then do not change the bg color
+  if(!(menu.classList.contains('show'))){
+    // Check if the top of the special area is visible in the viewport
+    if (faqsectionAreaPosition < navbar.clientHeight) {
+        // Change the background color when the special area is in view
+        navbar.style.backgroundColor = 'white';
+    } else {
+        // Reset the background color when the special area is out of view
+        navbar.style.backgroundColor = '';
+    }
+  }
+
+}
+
+
+
+
+// basic show and hide for faq section
 // const faqs = document.querySelectorAll(".faqsBtn");
 // faqs.forEach(faq => faq.addEventListener("click", (e)=>{
 //   content = faq.querySelector(".faqContent")
@@ -39,51 +166,7 @@ const swiper = new Swiper('.swiper', {
 
 // }));
 
-
-const faqs = document.querySelectorAll(".faqsBtn");
-// faqs.forEach(faq => faq.addEventListener("click", (e)=>{
-//   content = faq.querySelector(".faqContent")
-
-//   if (content.classList.contains('show')) {
-//     content.style.animationName = "hide";
-//     setTimeout(()=>{
-//       content.classList.remove('show');
-//     },500);
-    
-//   } else {
-//     content.style.animationName = "show";
-//     content.classList.add('show');
-//   }
-
-// }));
-
-
-faqs.forEach(faq => faq.addEventListener("click", (e)=>{
-  content = faq.querySelector(".faqContent")
-
-  if (content.classList.contains('show')) {
-    content.style.animationName = "menuHide";
-    setTimeout(()=>{
-      content.classList.remove('show');
-    },500);
-    faq.querySelector(".showContent").classList.remove('hidden');
-    faq.querySelector(".hideContent").classList.add('hidden');
-  } else {
-    content.style.animationName = "menuShow";
-    content.classList.add('show');
-    faq.querySelector(".showContent").classList.add('hidden');
-    faq.querySelector(".hideContent").classList.remove('hidden');
-  }
-
-}));
-
-// const faqs = document.querySelectorAll(".faqsBtn");
-// faqs.forEach(faq => faq.addEventListener("click", (e)=>{
-//   content = faq.querySelector(".faqContent");
-//   content.classList.toggle('show');
-
-// }));
-
+// basic hide and show for menu
 // const menuIcon = document.querySelector('#menuIcon');
 // const menu = document.querySelector('#menu');
 
@@ -96,30 +179,3 @@ faqs.forEach(faq => faq.addEventListener("click", (e)=>{
 //         menu.classList.add('hidden');
 //     }
 // });
-
-const menuIcon = document.querySelector('#menuIcon');
-const menu = document.querySelector('#menu');
-
-menuIcon.addEventListener('click', (e) => {
-    if (menu.classList.contains('show')) {
-
-      document.querySelector("#menu").style.animationName = "menuHide";
-      setTimeout(()=>{
-        menu.classList.remove('show');
-      },500);
-      
-    } else {
-      document.querySelector("#menu").style.animationName = "menuShow";
-      menu.classList.add('show');
-    }
-});
-
-window.addEventListener('resize', () => {
-  const screenWidth = window.innerWidth;
-  if(screenWidth >= 1280){
-    document.querySelector("#menu").style.animationName = "menuHide";
-    setTimeout(()=>{
-      menu.classList.remove('show');
-    },500);
-  }
-});
